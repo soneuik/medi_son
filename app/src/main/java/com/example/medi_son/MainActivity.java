3e9c5ec94794035b772d35cb86829e9437343e8d
soneu <dmldlr02>
2020-06-15 20:09:38 -0700

    classic buttons with songs

--- app/src/main/java/com/example/medi_son/MainActivity.java
+++ app/src/main/java/com/example/medi_son/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     ImageButton row2_1, row2_2, row2_3, row2_4, row2_5;
 
 
+    //Classic Img_btn
+    ImageButton c1_btn, c2_btn,c3_btn,c4_btn,c5_btn,c6_btn,c7_btn,c8_btn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +26,98 @@ public class MainActivity extends AppCompatActivity {
 
 
         row1_1 = (ImageButton)findViewById(R.id.row1_1);
+        row1_2 = (ImageButton)findViewById(R.id.row1_2);
+        row1_3 = (ImageButton)findViewById(R.id.row1_2);
+
+
+        c1_btn = (ImageButton)findViewById(R.id.c1_btn);
+        c2_btn = (ImageButton)findViewById(R.id.c2_btn);
+        c3_btn = (ImageButton)findViewById(R.id.c3_btn);
+
+        c4_btn = (ImageButton)findViewById(R.id.c4_btn);
+        c5_btn = (ImageButton)findViewById(R.id.c5_btn);
+        c6_btn = (ImageButton)findViewById(R.id.c6_btn);
+
+        c7_btn = (ImageButton)findViewById(R.id.c7_btn);
+        c8_btn = (ImageButton)findViewById(R.id.c8_btn);
 
 
 
-        row1_1.setOnClickListener(new View.OnClickListener() {
+        c1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopPlaying();
-                mp = MediaPlayer.create(MainActivity.this, R.raw.c_0);
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c1);
                 mp.start();
             }
         });
+
+
+        c2_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c2);
+                mp.start();
+            }
+        });
+
+        c3_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c3);
+                mp.start();
+            }
+        });
+
+        c4_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c4);
+                mp.start();
+            }
+        });
+
+        c5_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c5);
+                mp.start();
+            }
+        });
+
+        c6_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c6);
+                mp.start();
+            }
+        });
+
+        c7_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c7);
+                mp.start();
+            }
+        });
+
+        c8_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                mp = MediaPlayer.create(MainActivity.this, R.raw.c8);
+                mp.start();
+            }
+        });
+
+
+
 
 
 
